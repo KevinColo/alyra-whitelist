c299ab463ec58719f2aa247236e13db9958138a6
tpdq3633 <kevin.colonna@orange.com>
2022-03-29 11:21:01 +0200

    Updates

---
+++ precache-manifest.dc7095129b6420022d2b98314db26552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160edbd9c3764514757b4083e10462cb",
+    "url": "/alyra-whitelist/index.html"
+  },
+  {
+    "revision": "83b2de3ce49a5de6daec",
+    "url": "/alyra-whitelist/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "174256e8b12ce9d45785",
+    "url": "/alyra-whitelist/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "83b2de3ce49a5de6daec",
+    "url": "/alyra-whitelist/static/js/2.df1fe3f2.chunk.js"
+  },
+  {
+    "revision": "174256e8b12ce9d45785",
+    "url": "/alyra-whitelist/static/js/main.0ead87cf.chunk.js"
+  },
+  {
+    "revision": "68ac40e175fbb24f21c0",
+    "url": "/alyra-whitelist/static/js/runtime-main.09dc6773.js"
+  }
+]);
